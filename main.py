7a676a5aefaed930a3392a954e13d5d5ea361574
lijiapei <lipei10011@163.com>
2018-10-07 19:28:35 +0800

    国旗

--- main.py
+++ main.py
@@ -1,7 +1,68 @@
-for i in range(1,10):
-    for k in range(1,i):
-        print (end="        ")
-    for j in range(i,10):
-        print("{}x{}={:2} ".format(i,j,i*j),  end=' ')
-   
-    print()
+
+
+
+import turtle
+
+import turtle
+turtle.screensize(960,640,"red")
+turtle.fillcolor("yellow")
+turtle.color('yellow')
+
+#主星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-600,220) 
+turtle.down()
+for i in range (5):    
+    turtle.forward(150)
+    turtle.right(144)
+turtle.end_fill()
+
+#第1颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,295)
+turtle.setheading(305)
+turtle.down()
+for i in range (5):    
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
+
+
+#第2颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,212)
+turtle.setheading(30)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+
+#第3颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-350,145)
+turtle.setheading(5)
+turtle.down()
+for i in range (5):   
+    turtle.forward(50)
+    turtle.right(144)
+
+turtle.end_fill()
+
+#第4颗副星
+turtle.begin_fill()
+turtle.up()
+turtle.goto(-400,90)
+turtle.setheading(300)
+turtle.down()
+for i in range (5):  
+    turtle.forward(50)
+    turtle.left(144)
+
+turtle.end_fill()
